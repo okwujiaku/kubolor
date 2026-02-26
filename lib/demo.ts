52b4bac73d8ceb940e5d895cb86579bb479ea191
okwujiaku <valentineokwujiaku19@gmail.com>
2026-02-26 11:43:41 -0800

    Hide policy pages on live site

---
+++ lib/demo.ts
@@ -0,0 +1,3 @@
+/** Set NEXT_PUBLIC_SHOW_POLICY_PAGES=true to show policies (hidden by default on live site). */
+export const SHOW_POLICY_PAGES =
+  process.env.NEXT_PUBLIC_SHOW_POLICY_PAGES === "true";
